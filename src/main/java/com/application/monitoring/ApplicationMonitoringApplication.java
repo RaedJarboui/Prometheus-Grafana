b4f52ce6a3584e1af63b09e6207942e8c5fbd5da
Raed Jarboui <raedjarboui1998@gmail.com>
2024-11-15 16:59:15 +0100

    add prometheus configuration

---
+++ src/main/java/com/application/monitoring/ApplicationMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.application.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/monitoring/ApplicationMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
